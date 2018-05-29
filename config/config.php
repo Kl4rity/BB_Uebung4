df202a8b360d1b1290971e20226881b68dd9c87f
Clemens Stift <cstift@Clemenss-MacBook-Pro-3.local>
2018-05-29 14:27:50 +0200

    Changed order of imports to allow for interface to work in ElectricModelItem.php and to add ShoppingList.php

--- config/config.php
+++ config/config.php
@@ -4,12 +4,14 @@ error_reporting(E_ALL);
 include "controller/ListController.php";
 include "services/Database.php";
 include "views/JsonView.php";
+include "interfaces/iPostRequestExecutor.php";
 include "models/ElectricModelItem.php";
 include "models/Floor.php";
 include "models/Device.php";
 include "models/Project.php";
 include "models/Room.php";
 include "models/Sensor.php";
+include "models/ShoppingList.php";
 
 define ("DBHost", "localhost");
 define ("DBName", "uebung3");
